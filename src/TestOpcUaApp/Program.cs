7784c1c7e34561ed5553653dc767bf39316ec94b
sandervandevelde <svelde@gmail.com>
2025-01-24 20:29:13 +0100

    event node added

--- src/TestOpcUaApp/Program.cs
+++ src/TestOpcUaApp/Program.cs
@@ -8,93 +8,110 @@
     {
         public static void Main()
         {
+            // machine data variable nodes
+
             var temperatureNode = new OpcDataVariableNode<double>("Temperature", 100.0);
             var humidityNode = new OpcDataVariableNode<double>("Humidity", 50.0);
             var pressureNode = new OpcDataVariableNode<double>("Pressure", 10.0);
 
-            var machine = new OpcObjectNode( "Machine", temperatureNode, humidityNode, pressureNode);
+            var machine = new OpcObjectNode("Machine", temperatureNode, humidityNode, pressureNode);
 
-            using (var server = new OpcServer("opc.tcp://localhost:4840/", machine))
+            using var server = new OpcServer("opc.tcp://localhost:4840/", machine);
+
+            server.ApplicationName = "TestCSharpServer";
+            server.ApplicationUri = new Uri("http://test.csharpserver.uri/");
+            server.SessionCreated += (sender, e) => Console.Write($"+ {e.Session.Name} ");
+            server.SessionClosing += (sender, e) => Console.Write($"~ {e.Session.Name} ");
+            server.SessionClosed += (sender, e) => Console.Write($"-");
+
+            Console.WriteLine($"Starting OPC-UA server '{server.Address}'");
+
+            // server security
+
+            var acl = server.Security.UserNameAcl;
+            acl.AddEntry("unamecs", "psswrdCS!01");
+            acl.IsEnabled = true;
+
+            // machine alarm node
+
+            var alarmNode = new OpcEventNode(machine, "AlarmEvent");
+            alarmNode.Severity = OpcEventSeverity.Max;
+            machine.AddNotifier(server.SystemContext, alarmNode);
+
+            // start
+
+            server.Start();
+
+            Console.WriteLine($"Event '{alarmNode.Name}:{alarmNode.Id}'");
+
+            Console.WriteLine($"Datapoint '{temperatureNode.Name}:{temperatureNode.Id}'");
+            Console.WriteLine($"Datapoint '{humidityNode.Name}:{humidityNode.Id}'");
+            Console.WriteLine($"Datapoint '{pressureNode.Name}:{pressureNode.Id}'");
+
+            Console.Write($"Started at {DateTime.Now:HH:mm:ss}...");
+
+            var i = 0;
+
+            while (true)
             {
-                server.ApplicationName = "TestCSharpServer";
-                server.ApplicationUri = new Uri("http://test.csharpserver.uri/");
-                server.SessionCreated += Server_SessionCreated;
-                server.SessionClosed += Server_SessionClosed;
+                // temperature
 
-                Console.WriteLine($"Starting OPC-UA server '{server.Address}'");
-
-                var acl = server.Security.UserNameAcl;
-                acl.AddEntry("unamecs", "psswrdCS!01");
-                acl.IsEnabled = true;
-
-                server.Start();
-
-                Console.WriteLine($"Datapoint '{temperatureNode.Name}:{temperatureNode.Id}'");
-                Console.WriteLine($"Datapoint '{humidityNode.Name}:{humidityNode.Id}'");
-                Console.WriteLine($"Datapoint '{pressureNode.Name}:{pressureNode.Id}'");
-
-                Console.Write($"Started at {DateTime.Now:HH:mm:ss}...");
-
-                while (true)
+                if (temperatureNode.Value == 110)
                 {
-                    // temperature
+                    temperatureNode.Value = 100;
+                }
+                else
+                {
+                    temperatureNode.Value++;
+                }
 
-                    if (temperatureNode.Value == 110)
-                    {
-                        temperatureNode.Value = 100;
-                    }
-                    else
-                    {
-                        temperatureNode.Value++;
-                    }
+                temperatureNode.Timestamp = DateTime.UtcNow;
+                temperatureNode.ApplyChanges(server.SystemContext);
 
-                    temperatureNode.Timestamp = DateTime.UtcNow;
-                    temperatureNode.ApplyChanges(server.SystemContext);
+                // humidity
 
-                    // humidity
+                if (humidityNode.Value == 55)
+                {
+                    humidityNode.Value = 45;
+                }
+                else
+                {
+                    humidityNode.Value++;
+                }
 
-                    if (humidityNode.Value == 55)
-                    {
-                        humidityNode.Value = 45;
-                    }
-                    else
-                    {
-                        humidityNode.Value++;
-                    }
+                humidityNode.Timestamp = DateTime.UtcNow;
+                humidityNode.ApplyChanges(server.SystemContext);
 
-                    humidityNode.Timestamp = DateTime.UtcNow;
-                    humidityNode.ApplyChanges(server.SystemContext);
+                // pressure
 
-                    // pressure
+                if (pressureNode.Value == 15)
+                {
+                    pressureNode.Value = 5;
+                }
+                else
+                {
+                    pressureNode.Value++;
+                }
 
-                    if (pressureNode.Value == 15)
-                    {
-                        pressureNode.Value = 5;
-                    }
-                    else
-                    {
-                        pressureNode.Value++;
-                    }
+                pressureNode.Timestamp = DateTime.UtcNow;
+                pressureNode.ApplyChanges(server.SystemContext);
 
-                    pressureNode.Timestamp = DateTime.UtcNow;
-                    pressureNode.ApplyChanges(server.SystemContext);
+                // sleep
 
-                    // sleep
+                Console.Write(".");
+                Thread.Sleep(5000);
 
-                    Console.Write(".");
-                    Thread.Sleep(5000);
+                i++;
+
+                if (i % 2 == 0)
+                {
+                    alarmNode.Severity = OpcEventSeverity.Medium;
+                    alarmNode.Message = $"Urgent situation {i}";
+                    alarmNode.ReportEvent(server.SystemContext);
+
+                    Console.Write("!");
                 }
             }
-        }
-
-        private static void Server_SessionClosed(object? sender, EventArgs e)
-        {
-            Console.Write("-");
-        }
-
-        private static void Server_SessionCreated(object sender, OpcSessionEventArgs e)
-        {
-            Console.Write($"+ {e.Session.Name} ");
         }
     }
 }
--- src/TestOpcUaClientApp/Program.cs
+++ src/TestOpcUaClientApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Opc.UaFx;
+﻿using Microsoft.AspNetCore.Components;
+using Opc.UaFx;
 using Opc.UaFx.Client;
 using System.Reflection.PortableExecutable;
 
@@ -10,15 +11,34 @@ namespace TestOpcUaClientApp
         {
             Console.WriteLine("I am an OPC-UA client!");
 
-            var client = new OpcClient("opc.tcp://localhost:4840/");
+            using var client = new OpcClient("opc.tcp://localhost:4840/");
 
             client.Security.UserIdentity = new OpcClientIdentity("unamecs", "psswrdCS!01");
 
             client.Connect();
 
-//Temperature:ns=2;s=Machine/Temperature
-//Humidity:ns=2;s=Machine/Humidity
-//Pressure:ns=2;s=Machine/Pressure
+            //AlarmEvent:ns=2;s=Machine/AlarmEvent
+
+            var commands = new OpcSubscribeEvent[] {
+                
+
+                new OpcSubscribeEvent("ns=2;s=Machine", (sender, e) => 
+                    {
+                        Console.WriteLine($"Command ns=2;s=Machine {e.Event.SourceName} {e.Event.EventType}, {e.Event.Time}, {e.Event.Message}");
+                    }),
+                new OpcSubscribeEvent("ns=2;s=Machine/AlarmEvent", (sender, e) => 
+                    {
+                        Console.WriteLine($"Command ns=2;s=Machine/AlarmEvent {e.Event.SourceName} {e.Event.EventType}, {e.Event.Time}, {e.Event.Message}");
+                    })
+            };
+
+            var subscriptionCommands = client.SubscribeNodes(commands);
+
+            //Temperature:ns=2;s=Machine/Temperature
+            //Humidity:ns=2;s=Machine/Humidity
+            //Pressure:ns=2;s=Machine/Pressure
+
+            var i = 1;
 
             while (true)
             {
@@ -29,10 +49,17 @@ namespace TestOpcUaClientApp
                 Console.WriteLine($"temperature:{temperature}, humidity:{humidity}, pressure:{pressure}");
 
                 Thread.Sleep(1000);
+
+
+
+                i++;
+
+                if (i == 25)
+                {
+                    client.Disconnect();
+                    break;
+                }
             }
-
-
-
         }
     }
 }
